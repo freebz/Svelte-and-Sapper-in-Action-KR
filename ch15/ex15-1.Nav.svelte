157c012a54ef0a74052d302189e3f112b1a108f5
freebz <freebz@hananet.net>
2022-02-20 02:33:39 +0900

    15장 처음 만나는 새퍼 앱

---
+++ ch15/ex15-1.Nav.svelte
@@ -0,0 +1,59 @@
+<!-- 예제 15-1 src/components/Nav.svelte 파일의 Nav 컴포넌트 -->
+
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255,62,0,0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Shop</a></li>
+		<li><a aria-current="{segment === 'cart' ? 'page' : undefined}" href="cart">Cart</a></li>
+		<li><a aria-current="{segment === 'ship' ? 'page' : undefined}" href="ship">Ship</a></li>
+	</ul>
+</nav>
---
+++ ch15/sapper-app/src/routes/cart.svelte
@@ -0,0 +1,33 @@
+<!-- 예제 9-6 src/Cart.svelte 파일에 정의한 Cart 컴포넌트 -->
+
+<script>
+  import {cartStore} from '../stores';
+
+  let total =
+    $cartStore.reduce((acc, item) => acc + item.price * item.quantity, 0);
+</script>
+
+<h1>Cart</h1>
+
+{#if $cartStore.length === 0}
+  <div>empty</div>
+{:else}
+  <table>
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each $cartStore as item}
+        <tr>
+          <td>{item.description}</td>
+          <td>{item.quantity}</td>
+          <td>${item.price.toFixed(2)}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{/if}
