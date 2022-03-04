d8786cdeb2bfc8e928a3669ae96e9cb3c9a923b5
freebz <freebz@hananet.net>
2022-03-05 00:25:40 +0900

    부록 G 스노팩

---
+++ appendix G/exG-1.App.svelte
@@ -0,0 +1,23 @@
+<!-- 예제 G-1 src/App.svelte 파일에서 DateDisplay 컴포넌트 사용하기 -->
+
+<script>
+  import _ from 'lodash';
+  import DateDisplay from './DateDisplay';
+
+  let name = 'Snowpack';
+</script>
+
+<h1>Hello, {_.startCase(name)}!</h1>
+
+<label>
+  Name
+  <input bind:value={name}>
+</label>
+
+<DateDisplay />
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
---
+++ appendix G/exG-2.DateDisplay.svelte
@@ -0,0 +1,9 @@
+<!-- 예제 G-2 src/DateDisplay.svelte 파일의 DateDisplay 컴포넌트 -->
+
+<script>
+  import {format} from 'date-fns';
+</script>
+
+<div>
+  Today is {format(new Date(), 'MMM dd, yyyy')}.
+</div>
